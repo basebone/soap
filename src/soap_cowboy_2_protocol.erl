42286c94dc6729e6906e35c760f4f8a8ac718f97
Paolo Brizzolari <paolo.brizzolari@basebone.com>
2017-06-13 11:42:36 +0200

    Clean up change

--- src/soap_cowboy_2_protocol.erl
+++ src/soap_cowboy_2_protocol.erl
@@ -36,11 +36,6 @@
 -type cowboy_req() :: cowboy_req:req().
 -type cowboy_env() :: cowboy_middleware:env().
 
--record(state, {
-  env :: cowboy_middleware:env(),
-  handler = undefined :: undefined | module()
-}).
-
 %%% ============================================================================
 %%% Exported functions
 %%% ============================================================================
@@ -51,15 +46,10 @@
 %% This callback is expected to behave like a middleware and to return an
 %% updated req object and environment.
 -spec upgrade(Cowboy_req::cowboy_req(), Env::cowboy_env(),
-              Soap_handler::module(), Options::any()) -> {ok, cowboy_req(), cowboy_env()}. 
-upgrade(Cowboy_req, Env, _, {Handler, Options}) ->
-  Cowboy_state = #state{env = Env, handler = Handler},
-  case soap_server_handler:new_req(Handler, soap_cowboy_2_protocol, Options, Cowboy_req) of
-    {continue, Soap_req} ->
-      check_conformance(Soap_req, Cowboy_req, Cowboy_state, soap_cowboy_2_protocol);
-    {ok, _StatusCode, _Headers, _Body, _Server_req} = Error ->
-      make_response(Error, Cowboy_state, soap_cowboy_2_protocol)
-  end.
+              Soap_handler::module(), {Implementation_handler::module(), Options::any()}) -> {ok, cowboy_req(), cowboy_env()}. 
+upgrade(Cowboy_req, Env, Soap_handler, {Handler, Options}) ->
+  soap_cowboy_protocol:upgrade(Cowboy_req, Env, Soap_handler, 
+                               {Handler, Options}, cowboy_2, ?MODULE).
 
 enrich_req(Cowboy_req, Soap_req) ->
   Method = cowboy_req:method(Cowboy_req),
@@ -85,69 +75,3 @@ make_list(Binary) ->
 terminate(Cowboy_req, Env, Handler) ->
   Result = cowboy_handler:terminate(normal, Cowboy_req, [], Handler),
   {ok, Cowboy_req, [{result, Result}|Env]}.
-
-check_conformance(Soap_req, Cowboy_req, Cowboy_state, Version_module) ->
-  %% collect some information about the protocol, so that 
-  %% conformance can be checked.
-  Soap_req2 = Version_module:enrich_req(Cowboy_req, Soap_req),
-  case soap_server_handler:check_http_conformance(Soap_req2) of
-    {continue, Soap_req3} ->
-      handle_xml(Soap_req3, Cowboy_state, Version_module);
-    {ok, _StatusCode, _Headers, _Body, _Server_req} = Error ->
-      make_response(Error, Cowboy_state, Version_module)
-  end.
-
-handle_xml(Soap_req, Cowboy_state, Version_module) ->
-  Cowboy_req = soap_req:server_req(Soap_req),
-  {ok, Message, Cowboy_req2} = cowboy_req:read_body(Cowboy_req),
-  Soap_req2 = soap_req:set_server_req(Soap_req, Cowboy_req2),
-  Soap_req3 = soap_req:set_http_body(Soap_req2, Message),
-  Content_type = soap_req:content_type(Soap_req3),
-  %% get the soap message (Xml) from the request body
-  {Xml, Soap_req4} = case maybe_content_type(Content_type) of
-    "multipart/related" -> 
-        %% soap with attachments, the message is in the first part
-        try 
-          [{Mime_headers, Body} | Attachments] =
-            mime_decode(Message, Content_type),
-            {Body, 
-             soap_req:set_mime_headers(
-               soap_req:set_req_attachments(Soap_req3, Attachments), 
-               Mime_headers)}
-      catch
-        _Class:_Type ->
-          {Message, Soap_req3}
-      end;
-    _ ->
-      {Message, Soap_req3}
-  end,
-  Handler_resp = soap_server_handler:handle_message(Xml, Soap_req4),
-  make_response(Handler_resp, Cowboy_state, Version_module).
-
-maybe_content_type(undefined) ->
-  undefined;
-maybe_content_type(Content_type) ->
-  string:to_lower(lists:sublist(Content_type, 17)).
-
-
-mime_decode(Message, Content_type_header) ->
-  Mime_parameters = lists:nthtail(17, Content_type_header),
-  Parsed_parameters = soap_mime:parse_mime_parameters(Mime_parameters),
-  Boundary = proplists:get_value("boundary", Parsed_parameters),
-  soap_mime:decode(Message, list_to_binary(Boundary)).
-
-make_response({ok, StatusCode, Headers, Body, Cowboy_req}, #state{env = Env, handler = Handler}, Version_module) ->
-  Cowboy_req2 = set_headers(Headers, Cowboy_req),
-  Cowboy_req3 = cowboy_req:set_resp_body(Body, Cowboy_req2),
-  Version_module:respond(Cowboy_req3, Env, Handler, StatusCode). 
-
-set_headers(Headers, Cowboy_req) ->
-  lists:foldl(fun({Name, Value}, R) -> 
-                  cowboy_req:set_resp_header(to_binary(Name), Value, R)
-              end,
-              Cowboy_req, Headers).
-
-to_binary(N) when is_binary(N) ->
-  N;
-to_binary(N) when is_list(N) ->
-erlang:list_to_binary(N).
