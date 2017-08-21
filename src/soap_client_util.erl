0e18e66c2ad2d1306dd2da713086a868f52d818a
Paolo Brizzolari <paolo.brizzolari@basebone.com>
2017-08-21 10:39:50 +0200

    Checking Erlsom Error

--- src/soap_client_util.erl
+++ src/soap_client_util.erl
@@ -257,6 +257,7 @@ parse_mime(Message, Model, Http_status, Http_headers,
 
 parse_xml(Message, Model, Http_status, Http_headers, 
           Version, Ns, Handler, Attachments, HTTP_body) ->
+    io:format("Before Parse: ~p ~p ~p ~p",[Message, Model, Version, Ns]),
     try erlsom:parse_sax(Message, 
                          #p_state{model = Model, version = Version,
                                   soap_ns = Ns, state = start,
